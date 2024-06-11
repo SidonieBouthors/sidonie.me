5f385b0e34d04ba21085f7e150a305a496665cc1
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-06-11 15:24:42 +0200

    feat: add card grid system

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CardGrid from "@components/CardGrid";
 import RecipeCard from "@components/RecipeCard";
 import image from "@public/recipes/illus-chickpea-curry.jpg";
 import { useState } from "react";
@@ -8,48 +9,14 @@ import { Layouts, Responsive, WidthProvider } from "react-grid-layout";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 export default function Recipes() {
-  var [rowHeight, setRowHeight] = useState(0);
-  function onWidthChange(
-    containerWidth: number,
-    margin: [number, number],
-    cols: number,
-    containerPadding: [number, number]
-  ) {
-    // Padding default to margin if undefined
-    // Margin defaults to 10 if undefined
-    const defaultValue = 10;
-    const marginTotal =
-      margin == undefined ? defaultValue * (cols - 1) : margin[0] * (cols - 1);
-    const paddingTotal =
-      containerPadding == undefined
-        ? margin == undefined
-          ? defaultValue * 2
-          : margin[0] * 2
-        : containerPadding[0] * 2;
-    const h = (containerWidth - marginTotal - paddingTotal) / cols;
-    setRowHeight(h);
-  }
-
   return (
     <div>
       <h1>Recipes</h1>
-      <ResponsiveGridLayout
-        className="layout"
-        rowHeight={rowHeight}
-        breakpoints={{ gg: 2 }}
-        cols={{ gg: 4 }}
-        onWidthChange={onWidthChange}
-      >
-        <div key="1" data-grid={{ x: 0, y: 0, w: 1, h: 1, static: true }}>
-          <RecipeCard title="Test Card" imageUrl={image.src} altText="" />
-        </div>
-        <div key="2" data-grid={{ x: 1, y: 1, w: 1, h: 1, static: true }}>
-          <RecipeCard title="Test Card" imageUrl={image.src} altText="" />
-        </div>
-        <div key="3" data-grid={{ x: 2, y: 0, w: 1, h: 1, static: true }}>
-          <RecipeCard title="Test Card" imageUrl={image.src} altText="" />
-        </div>
-      </ResponsiveGridLayout>
+      <CardGrid>
+        {(Array.from({ length: 20 })).map((_, index) => (
+          <RecipeCard key={index} title="Test Card" imageUrl={image.src} altText="" />
+        ))}
+      </CardGrid>
     </div>
   );
 }
---
+++ components/CardGrid.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import { Layout, Layouts, Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+/**
+ * CardGrid component that displays a grid of cards with a responsive layout
+ */
+export default function CardGrid({
+  children,
+}: {
+  children: React.ReactElement[];
+}) {
+  // Grid format that defines the breakpoints and the number of columns for each breakpoint
+  // [breakpointName, breakPointSize, col]
+  const gridFormat: [string, number, number][] = [
+    ["xl", 1200, 4],
+    ["l", 1000, 3],
+    ["m", 400, 2],
+    ["s", 200, 1],
+  ];
+  const margin: [number, number] = [15, 15];
+  const containerPadding: [number, number] = [30, 30];
+
+  var items = children == undefined || children == null ? [] : children;
+  var [rowHeight, setRowHeight] = useState(0);
+
+  /**
+   * Calculates the row height based on
+   * the container width, margin, cols and container padding
+   * @param containerWidth
+   * @param margin
+   * @param cols
+   * @param containerPadding
+   */
+  function onWidthChange(
+    containerWidth: number,
+    margin: [number, number],
+    cols: number,
+    containerPadding: [number, number]
+  ) {
+    // Padding default to margin if undefined
+    // Margin defaults to 10 if undefined
+    const defaultValue = 10;
+    const marginTotal =
+      margin == undefined ? defaultValue * (cols - 1) : margin[0] * (cols - 1);
+    const paddingTotal =
+      containerPadding == undefined
+        ? margin == undefined
+          ? defaultValue * 2
+          : margin[0] * 2
+        : containerPadding[0] * 2;
+    const h = (containerWidth - marginTotal - paddingTotal) / cols;
+    setRowHeight(h);
+  }
+
+  /**
+   * Generates the layout for each breakpoint
+   * @returns layouts for each breakpoint
+   */
+  function makeLayout() {
+    var layouts: Layouts = {};
+    for (let j = 0; j < gridFormat.length; j++) {
+      var [breakpointName, breakPointSize, col] = gridFormat[j];
+      var layout: Layout[] = [];
+      for (let i = 0; i < items.length; i++) {
+        var x = i % col;
+        var y = Math.floor(i / col);
+        layout.push({ i: i.toString(), x: x, y: y, w: 1, h: 1, static: true });
+      }
+      layouts[breakpointName] = layout;
+    }
+    return layouts;
+  }
+
+  /**
+   * Generates the breakpoints for the responsive grid layout
+   * @returns breakpoints for the responsive grid layout
+   */
+  function makeBreakpoints() {
+    var breakpoints: { [key: string]: number } = {};
+    for (let i = 0; i < gridFormat.length; i++) {
+      var [breakpointName, breakPointSize, col] = gridFormat[i];
+      breakpoints[breakpointName] = breakPointSize;
+    }
+    return breakpoints;
+  }
+
+  /**
+   * Generates the number of columns for each breakpoint
+   * @returns column number for each breakpoint
+   */
+  function makeCols() {
+    var cols: { [key: string]: number } = {};
+    for (let i = 0; i < gridFormat.length; i++) {
+      var [breakpointName, breakPointSize, col] = gridFormat[i];
+      cols[breakpointName] = col;
+    }
+    return cols;
+  }
+
+  return (
+    <ResponsiveGridLayout
+      className="layout"
+      layouts={makeLayout()}
+      margin={margin}
+      containerPadding={containerPadding}
+      rowHeight={rowHeight}
+      breakpoints={makeBreakpoints()}
+      cols={makeCols()}
+      onWidthChange={onWidthChange}
+    >
+      {items.map((item, index) => (
+        <div key={index.toString()}>{item}</div>
+      ))}
+    </ResponsiveGridLayout>
+  );
+}
