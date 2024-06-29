cec40c20fee15ea1fa3bb912abf8c96c7aee39fa
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-06-29 20:22:26 +0200

    feat: error 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Error404() {
+  return (
+    <div className="error-page">
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <span className="error-number">404</span>
+      <div className="error-buttons">
+        <Link className="button" href="/">Back Home</Link>
+        <Link className="button" target="_blank" rel="noopener noreferrer" href="https://github.com/SidonieBouthors/sidonie.me-v2/issues/new?assignees=SidonieBouthors&labels=broken-link&projects=&template=broken-link.md&title=%5BBroken+Link%5D+">
+          Report Issue
+        </Link>
+      </div>
+    </div>
+  );
+}
