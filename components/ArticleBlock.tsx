84b2ce0a869fdaa1cabc3368d4dea763ddee2355
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2025-01-16 21:31:36 +0100

    feat: update article blocks to support tag disabling, flexing and changing title level

--- components/ArticleBlock.tsx
+++ components/ArticleBlock.tsx
@@ -14,29 +14,43 @@ interface ArticleBlockProps {
 
 export default function ArticleBlock({
   article: { slug, title, description, date, tags },
+  disableTags,
+  fillSpace,
+  titleLevel,
 }: {
   article: ArticleBlockProps;
+  disableTags?: boolean;
+  fillSpace?: boolean;
+  titleLevel?: number;
 }) {
   const isoDate = format(new Date(date), "yyyy-MM-dd");
   const formattedDate = format(new Date(date), "MMMM dd, yyyy");
 
   return (
-    <article className="article-block">
-      <Link href={slug}>
-        <h2>{title}</h2>
-      </Link>
-      <p>{description}</p>
-      {tags.length != 0 ? (
-        <div className="tags">
-          {tags?.map((tag) => (
-            <Tag key={tag} tag={tag} />
-          ))}
-        </div>
-      ) : null}
-      <span className="art-date">
-        <CalendarIcon />
-        {formattedDate}
-      </span>
+    <article className={`article-block ${fillSpace ? "fill-space" : ""}`}>
+      <div className="flex-group">
+        <Link href={slug}>
+          {React.createElement(
+            `h${titleLevel || 2}`,
+            { className: "article-block-title" },
+            title
+          )}
+        </Link>
+        <p>{description}</p>
+      </div>
+      <div className="flex-group">
+        {tags.length != 0 ? (
+          <div className="tags">
+            {tags?.map((tag) => (
+              <Tag key={tag} tag={tag} disabled={disableTags} />
+            ))}
+          </div>
+        ) : null}
+        <span className="art-date">
+          <CalendarIcon />
+          {formattedDate}
+        </span>
+      </div>
     </article>
   );
 }
