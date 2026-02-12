fd96450609aa2940b33771aa2e0fab97b58affd7
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2026-02-12 19:47:36 +0100

    chore: use turbopack compatible velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,16 @@
-import { build } from "velite";
-
 /** @type {import('next').NextConfig} */
+
+const isDev = process.argv.indexOf('dev') !== -1
+const isBuild = process.argv.indexOf('build') !== -1
+
+if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
+  process.env.VELITE_STARTED = '1'
+  const { build } = await import('velite')
+  await build({ watch: isDev, clean: !isDev })
+}
 
 const nextConfig = {
   output: "standalone",
-  webpack: (config) => {
-    config.plugins.push(new VeliteWebpackPlugin());
-    return config;
-  },
 };
 
 export default nextConfig;
-
-class VeliteWebpackPlugin {
-  static started = false;
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
-      if (VeliteWebpackPlugin.started) return;
-      VeliteWebpackPlugin.started = true;
-      const dev = compiler.options.mode === "development";
-      await build({ watch: dev, clean: !dev });
-    });
-  }
-}
