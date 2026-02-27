fe3f6af5cd0d3f93b600536c50c153a2cb177da9
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2026-02-27 18:34:04 +0100

    chore: fix velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isDev = process.argv.indexOf('dev') !== -1
-const isBuild = process.argv.indexOf('build') !== -1
-
-if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
-  process.env.VELITE_STARTED = '1'
-  const { build } = await import('velite')
-  await build({ watch: isDev, clean: !isDev })
-}
-
 const nextConfig = {
   output: "standalone",
 };
